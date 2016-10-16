b077edadf501e55f6dfb1d7a5f5e0ec806bb6178
xwa132 <xwa132@sfu.ca>
2016-10-15 17:49:43 -0700

    initial commit

---
+++ link_finder.py
@@ -0,0 +1,34 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+
+class LinkFinder(HTMLParser):
+
+    # Initialize class
+    def _init_(self, base_url, page_url):
+        super()._init_()
+        self.base_url = base_url
+        self.page_url = page_url
+        # Sorted, without duplicates
+        self.links = set()
+
+    # Overwrite built in method
+    def handle_starttag(self, tag, attrs):
+        # Only consider links
+        if tag == 'a':
+            # An attribute tupple is <href = link_value>
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    # Join relative url with base url
+                    # Do nothing if value is a full url
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+    # Output method
+    def page_links(self):
+        return self.links
+
+    def error(self, message):
+        pass
+
+  
